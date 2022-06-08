789f17288dfe7d566daf1d279ea9eb6ff3ac6b4a
MatheusPiva <matheusdspiva@gmail.com>
2022-06-08 20:07:52 -0300

    Troca Talentos - Front v1.0
    only login

---
+++ ProjetoVue/vue-project/src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <header>
+    <div class="header_app">Troca Talentos</div>
+  </header>
+  <div class="body_layout">
+    <RouterView />
+  </div>
+</template>
+
+<style>
+.body_layout {
+  background-color: #e9e9e9;
+}
+.header_app {
+  height: 10vh;
+  box-shadow: 0px 4px 16px 5px rgba(0,0,0,0.33) !important;
+  display: flex;
+  align-items: center;
+  padding-left: 30px;
+  font-weight: 600;
+}
+</style>
