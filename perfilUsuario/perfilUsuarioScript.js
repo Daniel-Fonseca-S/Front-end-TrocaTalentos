8b3a20d984d2ec0c8b0040283bcf917f30155da0
Sonic-Gale <daniel.fonseca2002@hotmail.com>
2022-06-07 21:58:11 -0300

    commit

--- perfilUsuario/perfilUsuarioScript.js
+++ perfilUsuario/perfilUsuarioScript.js
@@ -1,11 +1,13 @@
 let ipt = document.querySelector("input");
 
 function selectedInput(input) {
-    let tagp = document.createElement("p");
-    tagp.innerHTML = input.placeholder;
-    tagp.classList = "ms-3 font-monospace small-size-text bg-light negative-margin"
-    input.style.borderTop = "none";
-    input.parentNode.insertBefore(tagp, input)
+    let tagp = input.parentNode.querySelector("p");
+    if (tagp.innerHTML == "") {
+        tagp.innerHTML = input.placeholder;
+        tagp.classList = "ms-3 font-monospace small-size-text bg-light negative-margin"
+        input.style.borderTop = "none";
+        input.parentNode.insertBefore(tagp, input)
+    }
 }
 
 function unselectedInput(input) {
